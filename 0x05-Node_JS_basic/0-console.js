193d873ed720a1ef26ce140c63e7394e29e6a4c5
HoodaHashem <mahmoudhashem335@gmail.com>
2024-06-18 16:14:38 +0300

    first task!

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * display a message in the console
+ * @param {string} message
+ * @returns {void}
+ * @author: Gho5t
+*/
+const displayMessage = (message) => {
+  console.log(message);
+}
+
+module.exports = displayMessage;
